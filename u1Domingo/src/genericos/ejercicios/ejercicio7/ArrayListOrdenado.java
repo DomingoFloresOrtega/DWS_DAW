8f66fcc712b88f99fef1c641646ae4a1225d25fc
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-10-06 10:13:12 +0200

    Ejercicio 7

--- u1Domingo/src/genericos/ejercicios/ejercicio7/ArrayListOrdenado.java
+++
@@ -1,7 +0,0 @@
-package genericos.ejercicios.ejercicio7;
-
-import java.util.ArrayList;
-
-public class ArrayListOrdenado extends ArrayList {
-	
-}
---
+++ u1Domingo/src/genericos/ejercicios/ejercicio7/Operable.java
@@ -0,0 +1,8 @@
+package genericos.ejercicios.ejercicio7;
+
+public interface Operable<E> {
+	public void sumar(E o);
+	public void restar(E o);
+	public void multiplicar(E o);
+	public void dividir(E o);
+}
