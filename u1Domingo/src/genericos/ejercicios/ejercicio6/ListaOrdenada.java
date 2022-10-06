947e4eb4fbbb10c0cfe025ce30a5d3cbbb2b970a
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-10-06 10:13:01 +0200

    Ejercicio 6

--- u1Domingo/src/genericos/ejercicios/ejercicio6/ListaOrdenada.java
+++ u1Domingo/src/genericos/ejercicios/ejercicio6/ListaOrdenada.java
@@ -1,5 +1,36 @@
 package genericos.ejercicios.ejercicio6;
 
-public class ListaOrdenada<T> {
+import java.util.ArrayList;
+
+public abstract class ListaOrdenada<E extends Comparable<E>> {
 	
+	ArrayList<E> lista = new ArrayList<E>();
+	
+	public ListaOrdenada(Comparable<E> e) {
+		this.lista = lista;
+	}
+	
+	public void add (E o) {
+		lista.add(o);
+	}
+	
+	public int size() {
+		return lista.size();
+	}
+	
+	public boolean isEmpty() {
+		return lista.isEmpty();
+	}
+	
+	public boolean remove(E o) {
+		return lista.remove(o);
+	}
+	
+	public int indexOf(E o) {
+		return lista.indexOf(o);
+	}
+	
+	public String toString() {
+		return lista.toString();
+	}
 }
