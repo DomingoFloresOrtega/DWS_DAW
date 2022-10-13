a4a6a7600b185d00aea9af156a02c838b81025a5
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-10-13 09:56:03 +0200

    Teoría sobre lambdas (13/10/2022)

---
+++ u1Domingo/src/lambdas/teoria/Operacion.java
@@ -0,0 +1,7 @@
+package lambdas.teoria;
+
+public interface Operacion {
+	
+	public int oper(int i1, int i2);
+
+}
---
+++ u1Domingo/src/lambdas/teoria/PruebaFuncional.java
@@ -0,0 +1,26 @@
+package lambdas.teoria;
+
+public class PruebaFuncional {
+	
+	public static void main(String[] args) {
+		
+		Operacion op = (j,i) -> j - i;
+		
+		System.out.println(op.oper(2, 1));
+		PruebaFuncional.imprimoOp(op);
+		PruebaFuncional.imprimoOp((j,i) -> j * i);
+		
+		PruebaFuncional.imprimoOp((j,i) -> j - i);
+		PruebaFuncional.imprimoOp(PruebaFuncional::resta);
+		
+	}
+	
+	public static void imprimoOp(Operacion op) {
+		System.out.println(op.oper(3, 1));
+	}
+	
+	public static int resta(int i1, int i2) {
+		return i1 - i2;
+	}
+	
+}
