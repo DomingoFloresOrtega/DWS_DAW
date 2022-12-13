9041e30580d5dc45e57905c2f6881b02db819ad2
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-12-13 12:29:47 +0100

    Clase ClienteService

---
+++ u3Domingo/spring_boot_web_mvc_jdbc_ventas/src/main/java/org/iesvdm/service/ClienteService.java
@@ -0,0 +1,26 @@
+package org.iesvdm.service;
+
+import java.util.List;
+
+import org.iesvdm.dao.ClienteDAO;
+import org.iesvdm.modelo.Cliente;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+	
+	private ClienteDAO clienteDAO;
+	
+	//Se utiliza inyección automática por constructor del framework Spring.
+	//Por tanto, se puede omitir la anotación Autowired
+	//@Autowired
+	public ClienteService(ClienteDAO clienteDAO) {
+		this.clienteDAO = clienteDAO;
+	}
+	
+	public List<Cliente> listAll() {
+		
+		return clienteDAO.getAll();
+		
+	}
+}
