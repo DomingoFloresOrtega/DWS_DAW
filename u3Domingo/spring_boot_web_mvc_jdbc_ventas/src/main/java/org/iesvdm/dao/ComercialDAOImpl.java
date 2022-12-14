0660196bcab2c9bc444eaff63b5b34a459bf43f1
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-12-14 10:35:03 +0100

    Cambios en implementación de ComercialDAO

--- u3Domingo/spring_boot_web_mvc_jdbc_ventas/src/main/java/org/iesvdm/dao/ComercialDAOImpl.java
+++ u3Domingo/spring_boot_web_mvc_jdbc_ventas/src/main/java/org/iesvdm/dao/ComercialDAOImpl.java
@@ -1,11 +1,14 @@
 package org.iesvdm.dao;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 
 import org.iesvdm.modelo.Cliente;
 import org.iesvdm.modelo.Comercial;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import lombok.AllArgsConstructor;
@@ -19,48 +22,81 @@ public class ComercialDAOImpl implements ComercialDAO {
 
 	// JdbcTemplate se inyecta por el constructor de la clase automaticamente
 	private JdbcTemplate jdbcTemplate;
-	
-	
-	
+
 	@Override
 	public void create(Comercial comercial) {
-		// TODO Auto-generated method stub
+		String sqlInsert = """
+				INSERT INTO comercial (nombre, apellido1, apellido2, comisión)
+				VALUES  (     ?,         ?,         ?,       ?)
+				  """;
+
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+
+		// Con recuperación de id generado
+		int rows = jdbcTemplate.update(connection -> {
+			PreparedStatement ps = connection.prepareStatement(sqlInsert, new String[] { "id" });
+			int idx = 1;
+			ps.setString(idx++, comercial.getNombre());
+			ps.setString(idx++, comercial.getApellido1());
+			ps.setString(idx++, comercial.getApellido2());
+			ps.setFloat(idx++, comercial.getComision());
+			return ps;
+		}, keyHolder);
+
+		comercial.setId(keyHolder.getKey().intValue());
+
+		log.info("Insertados {} registros.", rows);
 
 	}
 
 	@Override
 	public List<Comercial> getAll() {
-		
-		List<Comercial> listCom = jdbcTemplate.query(
-                "SELECT * FROM comercial",
-                (rs, rowNum) -> new Comercial(rs.getInt("id"),
-                						 	rs.getString("nombre"),
-                						 	rs.getString("apellido1"),
-                						 	rs.getString("apellido2"),
-                						 	rs.getFloat("comisión")
-                						 	)
-        );
-		
+
+		List<Comercial> listCom = jdbcTemplate.query("SELECT * FROM comercial",
+				(rs, rowNum) -> new Comercial(rs.getInt("id"), rs.getString("nombre"), rs.getString("apellido1"),
+						rs.getString("apellido2"), rs.getFloat("comisión")));
+
 		log.info("Devueltos {} registros.", listCom.size());
-		
-        return listCom;
+
+		return listCom;
 	}
 
 	@Override
 	public Optional<Comercial> find(int id) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		Comercial com = jdbcTemplate.queryForObject("SELECT * FROM comercial WHERE id = ?",
+				(rs, rowNum) -> new Comercial(rs.getInt("id"), rs.getString("nombre"), rs.getString("apellido1"),
+						rs.getString("apellido2"), rs.getFloat("comisión")),
+				id);
+
+		if (com != null) {
+			return Optional.of(com);
+		} else {
+			log.info("Comercial no encontrado.");
+			return Optional.empty();
+		}
 	}
 
 	@Override
 	public void update(Comercial comercial) {
-		// TODO Auto-generated method stub
+		int rows = jdbcTemplate.update("""
+				UPDATE comercial SET
+								nombre = ?,
+								apellido1 = ?,
+								apellido2 = ?,
+								comisión = ?
+						WHERE id = ?
+				""", comercial.getNombre(), comercial.getApellido1(), comercial.getApellido2(), comercial.getComision(), 
+				comercial.getId());
+
+		log.info("Update de Comercial con {} registros actualizados.", rows);
 
 	}
 
 	@Override
 	public void delete(int id) {
-		// TODO Auto-generated method stub
+		int rows = jdbcTemplate.update("DELETE FROM comercial WHERE id = ?", id);
+		
+		log.info("Delete de Comercial con {} registros eliminados.", rows);
 
 	}
 
