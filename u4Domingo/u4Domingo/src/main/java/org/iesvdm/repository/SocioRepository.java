ba6d95a60378f041b7fbee2995fe6b4c57e90888
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2023-02-15 12:08:38 +0100

    Agregada anotacion

--- u4Domingo/u4Domingo/src/main/java/org/iesvdm/repository/SocioRepository.java
+++ u4Domingo/u4Domingo/src/main/java/org/iesvdm/repository/SocioRepository.java
@@ -2,6 +2,8 @@ package org.iesvdm.repository;
 
 import org.iesvdm.domain.Socio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SocioRepository extends JpaRepository<Socio, Long> {
 }
