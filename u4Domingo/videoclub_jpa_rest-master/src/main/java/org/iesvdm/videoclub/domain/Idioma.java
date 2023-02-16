e914d93dabdd1500b19124e26b9c6b7fc9c86422
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2023-02-16 08:46:28 +0100

    Repositorio base de videoclub

---
+++ u4Domingo/videoclub_jpa_rest-master/src/main/java/org/iesvdm/videoclub/domain/Idioma.java
@@ -0,0 +1,43 @@
+package org.iesvdm.videoclub.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name="idioma")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+//Si utilizo @OneToMany(FetchType.LAZY) además debo usar
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Idioma {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_idioma")
+    private Long id;
+    private String nombre;
+
+    @Column(name = "ultima_actualizacion")
+    @JsonFormat(pattern = "yyyy-MM-dd-HH:mm:ss",  shape = JsonFormat.Shape.STRING)
+    private Date ultimaActualizacion;
+
+    @OneToMany(mappedBy = "idioma")
+    @JsonIgnore
+    private List<Pelicula> peliculasIdioma;
+
+    @OneToMany(mappedBy = "idiomaOriginal")
+    @JsonIgnore
+    private List<Pelicula> peliculasIdiomaOriginal;
+}
---
+++ u4Domingo/videoclub_jpa_rest-master/src/main/java/org/iesvdm/videoclub/service/PeliculaService.java
@@ -0,0 +1,46 @@
+package org.iesvdm.videoclub.service;
+
+import org.iesvdm.videoclub.domain.Pelicula;
+import org.iesvdm.videoclub.exception.PeliculaNotFoundException;
+import org.iesvdm.videoclub.repository.PeliculaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PeliculaService {
+
+    private final PeliculaRepository peliculaRepository;
+
+    public PeliculaService(PeliculaRepository peliculaRepository) {
+        this.peliculaRepository = peliculaRepository;
+    }
+
+    public List<Pelicula> all() {
+        return this.peliculaRepository.findAll();
+    }
+
+    public Pelicula save(Pelicula pelicula) {
+        return this.peliculaRepository.save(pelicula);
+    }
+
+    public Pelicula one(Long id) {
+        return this.peliculaRepository.findById(id)
+                .orElseThrow(() -> new PeliculaNotFoundException(id));
+    }
+
+    public Pelicula replace(Long id, Pelicula pelicula) {
+
+        return this.peliculaRepository.findById(id).map( p -> (id.equals(pelicula.getIdPelicula())  ?
+                                                            this.peliculaRepository.save(pelicula) : null))
+                .orElseThrow(() -> new PeliculaNotFoundException(id));
+
+    }
+
+    public void delete(Long id) {
+        this.peliculaRepository.findById(id).map(p -> {this.peliculaRepository.delete(p);
+                                                        return p;})
+                .orElseThrow(() -> new PeliculaNotFoundException(id));
+    }
+
+}
